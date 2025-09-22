9c97648e8168151a040b7e98c1702c6f8c7b0002
P.RITESH CHANDRA <ritesh.chandra122@gmail.com>
2025-09-22 22:06:08 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,13 +1,1 @@
-import pandas as pd
-df=pd.read.csv("Titanic-Dataset.csv")
-print(df.head())
-print(df.info())
 
-# Step 4: Summary of numerical columns
-print(df.describe())
-
-# Step 5: Count missing values in each column
-print(df.isnull().sum())
-
-# Step 6: Check dataset shape (rows, columns)
-print("Shape of dataset:", df.shape)
